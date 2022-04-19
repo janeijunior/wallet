a1f29a00e8d6c4aba11510e80a508a61ebcc46a5
Janei Araujo <jaraujo.php@gmail.com>
2022-04-19 10:12:52 -0300

    ajustando painel para dashboard

--- application/views/sistema/painel.php
+++ application/views/sistema/painel.php
@@ -1,1 +1,80 @@
-test
+<div class="row row-stat">
+    <div class="col-md-4">
+        <div class="panel panel-success-alt noborder">
+            <div class="panel-heading noborder">
+                <div class="panel-btns">
+                    <a href="" class="panel-close tooltips" data-toggle="tooltip" title="Close Panel"><i class="fa fa-times"></i></a>
+                </div><!-- panel-btns -->
+                
+                <div class="media-body">
+                    <h5 class="md-title nomargin">Carteira Principal</h5>
+                    <h1 class="mt5">8.102,32</h1>
+                </div><!-- media-body -->
+                <hr>
+                <div class="clearfix mt20">
+                    <div class="pull-left">
+                        <h5 class="md-title nomargin">Yesterday</h5>
+                        <h4 class="nomargin">$29,009.17</h4>
+                    </div>
+                    <div class="pull-right">
+                        <h5 class="md-title nomargin"></h5>
+                        <h4 class="nomargin">BRL</h4>
+                    </div>
+                </div>
+
+            </div><!-- panel-body -->
+        </div><!-- panel -->
+    </div><!-- col-md-4 -->
+
+    <div class="col-md-4">
+        <div class="panel panel-primary noborder">
+            <div class="panel-heading noborder">
+                <div class="panel-btns">
+                    <a href="" class="panel-close tooltips" data-toggle="tooltip" title="Close Panel"><i class="fa fa-times"></i></a>
+                </div><!-- panel-btns -->
+                <div class="media-body">
+                    <h5 class="md-title nomargin">Carteira Adicional</h5>
+                    <h1 class="mt5">2,75</h1>
+                </div><!-- media-body -->
+                <hr>
+                <div class="clearfix mt20">
+                    <div class="pull-left">
+                        <h5 class="md-title nomargin">Yesterday</h5>
+                        <h4 class="nomargin">10,009</h4>
+                    </div>
+                    <div class="pull-right">
+                        <h5 class="md-title nomargin"></h5>
+                        <h4 class="nomargin">USD</h4>
+                    </div>
+                </div>
+
+            </div><!-- panel-body -->
+        </div><!-- panel -->
+    </div><!-- col-md-4 -->
+
+    <div class="col-md-4">
+        <div class="panel panel-dark noborder">
+            <div class="panel-heading noborder">
+                <div class="panel-btns">
+                    <a href="" class="panel-close tooltips" data-toggle="tooltip" data-placement="left" title="Close Panel"><i class="fa fa-times"></i></a>
+                </div><!-- panel-btns -->
+                <div class="media-body">
+                    <h5 class="md-title nomargin">Cotação atual</h5>
+                    <h1 class="mt5">R$4,66</h1>
+                </div><!-- media-body -->
+                <hr>
+                <div class="clearfix mt20">
+                    <div class="pull-left">
+                        <h5 class="md-title nomargin">Moeda</h5>
+                        <h4 class="nomargin">USD/BRL</h4>
+                    </div>
+                    <div class="pull-right">
+                        <h5 class="md-title nomargin">Atualizado</h5>
+                        <h4 class="nomargin">18/04/2022 - 15:03</h4>
+                    </div>
+                </div>
+
+            </div><!-- panel-body -->
+        </div><!-- panel -->
+    </div><!-- col-md-4 -->
+</div><!-- row -->
