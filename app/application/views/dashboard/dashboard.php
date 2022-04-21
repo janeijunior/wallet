24555a584de81465e766eb6ace28eff6bc326095
Janei Araujo <jaraujo.php@gmail.com>
2022-04-21 16:30:59 -0300

    adicionando painel como dashboard

--- app/application/views/dashboard/dashboard.php
+++ app/application/views/dashboard/dashboard.php
@@ -1,126 +1,80 @@
-<div class="box">
-    <div class="box-header with-border">
-        <h3 class="box-title">Monthly Recap Report</h3>
-
-        <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-            </button>
-            <div class="btn-group">
-                <button type="button" class="btn btn-box-tool dropdown-toggle" data-toggle="dropdown">
-                    <i class="fa fa-wrench"></i></button>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                </ul>
-            </div>
-            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-        </div>
-    </div>
-    <!-- /.box-header -->
-    <div class="box-body">
-        <div class="row">
-            <div class="col-md-8">
-                <p class="text-center">
-                    <strong>Sales: 1 Jan, 2014 - 30 Jul, 2014</strong>
-                </p>
-
-                <div class="chart">
-                    <!-- Sales Chart Canvas -->
-                    <canvas id="salesChart" style="height: 180px;"></canvas>
-                </div>
-                <!-- /.chart-responsive -->
-            </div>
-            <!-- /.col -->
-            <div class="col-md-4">
-                <p class="text-center">
-                    <strong>Goal Completion</strong>
-                </p>
-
-                <div class="progress-group">
-                    <span class="progress-text">Add Products to Cart</span>
-                    <span class="progress-number"><b>160</b>/200</span>
-
-                    <div class="progress sm">
-                        <div class="progress-bar progress-bar-aqua" style="width: 80%"></div>
+<div class="row row-stat">
+    <div class="col-md-4">
+        <div class="panel panel-success-alt noborder">
+            <div class="panel-heading noborder">
+                <div class="panel-btns">
+                    <a href="" class="panel-close tooltips" data-toggle="tooltip" title="Close Panel"><i class="fa fa-times"></i></a>
+                </div><!-- panel-btns -->
+                
+                <div class="media-body">
+                    <h5 class="md-title nomargin">Carteira Principal</h5>
+                    <h1 class="mt5">8.102,32</h1>
+                </div><!-- media-body -->
+                <hr>
+                <div class="clearfix mt20">
+                    <div class="pull-left">
+                        <h5 class="md-title nomargin"></h5>
+                        <h4 class="nomargin"></h4>
+                    </div>
+                    <div class="pull-right">
+                        <h5 class="md-title nomargin"></h5>
+                        <h4 class="nomargin">BRL</h4>
                     </div>
                 </div>
-                <!-- /.progress-group -->
-                <div class="progress-group">
-                    <span class="progress-text">Complete Purchase</span>
-                    <span class="progress-number"><b>310</b>/400</span>
 
-                    <div class="progress sm">
-                        <div class="progress-bar progress-bar-red" style="width: 80%"></div>
+            </div><!-- panel-body -->
+        </div><!-- panel -->
+    </div><!-- col-md-4 -->
+
+    <div class="col-md-4">
+        <div class="panel panel-primary noborder">
+            <div class="panel-heading noborder">
+                <div class="panel-btns">
+                    <a href="" class="panel-close tooltips" data-toggle="tooltip" title="Close Panel"><i class="fa fa-times"></i></a>
+                </div><!-- panel-btns -->
+                <div class="media-body">
+                    <h5 class="md-title nomargin">Carteira Adicional</h5>
+                    <h1 class="mt5">2,75</h1>
+                </div><!-- media-body -->
+                <hr>
+                <div class="clearfix mt20">
+                    <div class="pull-left">
+                        <h5 class="md-title nomargin"></h5>
+                        <h4 class="nomargin"></h4>
+                    </div>
+                    <div class="pull-right">
+                        <h5 class="md-title nomargin"></h5>
+                        <h4 class="nomargin">USD</h4>
                     </div>
                 </div>
-                <!-- /.progress-group -->
-                <div class="progress-group">
-                    <span class="progress-text">Visit Premium Page</span>
-                    <span class="progress-number"><b>480</b>/800</span>
 
-                    <div class="progress sm">
-                        <div class="progress-bar progress-bar-green" style="width: 80%"></div>
+            </div><!-- panel-body -->
+        </div><!-- panel -->
+    </div><!-- col-md-4 -->
+
+    <div class="col-md-4">
+        <div class="panel panel-dark noborder">
+            <div class="panel-heading noborder">
+                <div class="panel-btns">
+                    <a href="" class="panel-close tooltips" data-toggle="tooltip" data-placement="left" title="Close Panel"><i class="fa fa-times"></i></a>
+                </div><!-- panel-btns -->
+                <div class="media-body">
+                    <h5 class="md-title nomargin">Cotação atual</h5>
+                    <h1 class="mt5">R$4,66</h1>
+                </div><!-- media-body -->
+                <hr>
+                <div class="clearfix mt20">
+                    <div class="pull-left">
+                        <h5 class="md-title nomargin">Moeda</h5>
+                        <h4 class="nomargin">USD/BRL</h4>
+                    </div>
+                    <div class="pull-right">
+                        <h5 class="md-title nomargin">Atualizado</h5>
+                        <h4 class="nomargin">18/04/2022 - 15:03</h4>
                     </div>
                 </div>
-                <!-- /.progress-group -->
-                <div class="progress-group">
-                    <span class="progress-text">Send Inquiries</span>
-                    <span class="progress-number"><b>250</b>/500</span>
 
-                    <div class="progress sm">
-                        <div class="progress-bar progress-bar-yellow" style="width: 80%"></div>
-                    </div>
-                </div>
-                <!-- /.progress-group -->
-            </div>
-            <!-- /.col -->
-        </div>
-        <!-- /.row -->
-    </div>
-    <!-- ./box-body -->
-    <div class="box-footer">
-        <div class="row">
-            <div class="col-sm-3 col-xs-6">
-                <div class="description-block border-right">
-                    <span class="description-percentage text-green"><i class="fa fa-caret-up"></i> 17%</span>
-                    <h5 class="description-header">$35,210.43</h5>
-                    <span class="description-text">TOTAL REVENUE</span>
-                </div>
-                <!-- /.description-block -->
-            </div>
-            <!-- /.col -->
-            <div class="col-sm-3 col-xs-6">
-                <div class="description-block border-right">
-                    <span class="description-percentage text-yellow"><i class="fa fa-caret-left"></i> 0%</span>
-                    <h5 class="description-header">$10,390.90</h5>
-                    <span class="description-text">TOTAL COST</span>
-                </div>
-                <!-- /.description-block -->
-            </div>
-            <!-- /.col -->
-            <div class="col-sm-3 col-xs-6">
-                <div class="description-block border-right">
-                    <span class="description-percentage text-green"><i class="fa fa-caret-up"></i> 20%</span>
-                    <h5 class="description-header">$24,813.53</h5>
-                    <span class="description-text">TOTAL PROFIT</span>
-                </div>
-                <!-- /.description-block -->
-            </div>
-            <!-- /.col -->
-            <div class="col-sm-3 col-xs-6">
-                <div class="description-block">
-                    <span class="description-percentage text-red"><i class="fa fa-caret-down"></i> 18%</span>
-                    <h5 class="description-header">1200</h5>
-                    <span class="description-text">GOAL COMPLETIONS</span>
-                </div>
-                <!-- /.description-block -->
-            </div>
-        </div>
-        <!-- /.row -->
-    </div>
-    <!-- /.box-footer -->
-</div>
-<!-- /.box -->
+            </div><!-- panel-body -->
+        </div><!-- panel -->
+    </div><!-- col-md-4 -->
+</div><!-- row -->
