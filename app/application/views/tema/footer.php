0de040f7016e6e11506502dcd31deb442b0a5c19
Janei Araujo <jaraujo.php@gmail.com>
2022-04-21 16:31:28 -0300

    Adicionando view footer

---
+++ app/application/views/tema/footer.php
@@ -0,0 +1,21 @@
+<style type="text/css">
+  #footer{    
+    position:absolute;
+    bottom:0; 
+    width:100%;
+    margin-top: 0px;
+
+    }
+</style>
+
+
+<div class="panel panel-default" id="footer" style="margin-top:0px">
+    <div class="panel-body">
+        <div class="pull-right">
+            Page rendered in {elapsed_time} seconds | Versão <strong><?php echo $this->config->item('app_version'); ?></strong>
+        </div>
+        Copyright &copy; <?php echo date('Y'); ?> - <a href="https://safecortex.com.br/">SafeCortex</a>
+    </div>
+</div><!-- panel -->
+
+
