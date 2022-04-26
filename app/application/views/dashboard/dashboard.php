9f55466a529f0a9cd12f53773cbc2d41bc7cab6b
Janei Araujo <jaraujo.php@gmail.com>
2022-04-25 21:24:11 -0300

    ajustes de visualização no dashboard inicial

--- app/application/views/dashboard/dashboard.php
+++ app/application/views/dashboard/dashboard.php
@@ -5,20 +5,20 @@
                 <div class="panel-btns">
                     <a href="" class="panel-close tooltips" data-toggle="tooltip" title="Close Panel"><i class="fa fa-times"></i></a>
                 </div><!-- panel-btns -->
-                
+
                 <div class="media-body">
                     <h5 class="md-title nomargin">Carteira Principal</h5>
-                    <h1 class="mt5">8.102,32</h1>
+                    <h1 class="mt5">R$8.102,32</h1>
                 </div><!-- media-body -->
                 <hr>
                 <div class="clearfix mt20">
                     <div class="pull-left">
-                        <h5 class="md-title nomargin"></h5>
-                        <h4 class="nomargin"></h4>
+                        <h5 class="md-title nomargin">Moeda</h5>
+                        <h4 class="nomargin">BRL</h4>
                     </div>
                     <div class="pull-right">
                         <h5 class="md-title nomargin"></h5>
-                        <h4 class="nomargin">BRL</h4>
+                        <h4 class="nomargin"></h4>
                     </div>
                 </div>
 
@@ -34,17 +34,17 @@
                 </div><!-- panel-btns -->
                 <div class="media-body">
                     <h5 class="md-title nomargin">Carteira Adicional</h5>
-                    <h1 class="mt5">2,75</h1>
+                    <h1 class="mt5">$2,75</h1>
                 </div><!-- media-body -->
                 <hr>
                 <div class="clearfix mt20">
                     <div class="pull-left">
-                        <h5 class="md-title nomargin"></h5>
-                        <h4 class="nomargin"></h4>
+                        <h5 class="md-title nomargin">Moeda</h5>
+                        <h4 class="nomargin">USD</h4>
                     </div>
                     <div class="pull-right">
                         <h5 class="md-title nomargin"></h5>
-                        <h4 class="nomargin">USD</h4>
+                        <h4 class="nomargin"></h4>
                     </div>
                 </div>
 
@@ -60,7 +60,7 @@
                 </div><!-- panel-btns -->
                 <div class="media-body">
                     <h5 class="md-title nomargin">Cotação atual</h5>
-                    <h1 class="mt5">R$4,66</h1>
+                    <h1 class="mt5">1 USD = 4,7967 BRL</h1>
                 </div><!-- media-body -->
                 <hr>
                 <div class="clearfix mt20">
@@ -77,4 +77,157 @@
             </div><!-- panel-body -->
         </div><!-- panel -->
     </div><!-- col-md-4 -->
+
 </div><!-- row -->
+<br>
+<div class="row row-stat">
+    <div class="col-md-8">
+        <div class="panel panel-success-alt noborder">
+            <div class="panel-heading noborder">
+                <div class="panel-btns">
+                    <a href="" class="panel-close tooltips" data-toggle="tooltip" title="Close Panel"><i class="fa fa-times"></i></a>
+                </div><!-- panel-btns -->
+
+                <div class="media-body">
+                    <h5 class="md-title nomargin">Carteira Principal</h5>
+                    <h1 class="mt5">R$8.102,32</h1>
+                </div><!-- media-body -->
+                <hr>
+                <div class="clearfix mt20">
+                    <div class="pull-left">
+                        <h5 class="md-title nomargin">Moeda</h5>
+                        <h4 class="nomargin">BRL</h4>
+                    </div>
+                    <div class="pull-right">
+                        <h5 class="md-title nomargin"></h5>
+                        <h4 class="nomargin"></h4>
+                    </div>
+                </div>
+
+            </div><!-- panel-body -->
+        </div><!-- panel -->
+    </div><!-- col-md-4 -->
+
+    <div class="col-md-4">
+        <div class="panel panel-dark noborder">
+            <div class="panel-heading noborder">
+                <div class="panel-btns">
+                    <a href="" class="panel-close tooltips" data-toggle="tooltip" data-placement="left" title="Close Panel"><i class="fa fa-times"></i></a>
+                </div><!-- panel-btns -->
+                <div class="media-body">
+                    <h5 class="md-title nomargin">Cotação atual</h5>
+                    <h1 class="mt5">1 USD = 4,7967 BRL</h1>
+                </div><!-- media-body -->
+                <hr>
+                <div class="clearfix mt20">
+                    <div class="pull-left">
+                        <h5 class="md-title nomargin">Moeda</h5>
+                        <h4 class="nomargin">USD/BRL</h4>
+                    </div>
+                    <div class="pull-right">
+                        <h5 class="md-title nomargin">Atualizado</h5>
+                        <h4 class="nomargin">18/04/2022 - 15:03</h4>
+                    </div>
+                </div>
+
+            </div><!-- panel-body -->
+        </div><!-- panel -->
+    </div><!-- col-md-4 -->
+
+</div><!-- row -->
+<br>
+<div class="row">
+    <div class="col-lg-12">
+
+        <div class="panel panel-default">
+
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h5 class="lg-title mb5">Últimas transações</h5>
+                        <p class="mb20"></p>
+                        <div class="table-responsive">
+                            <table class="table mb30">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Data</th>
+                                        <th>Tipo</th>
+                                        <th>Status</th>
+                                        <th>Valor</th>
+                                        <th>Ação</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>24/01/2022 - 22:52</td>
+                                        <td>Depósito</td>
+                                        <td>Aprovado</td>
+                                        <td>US$ 100,00</td>
+                                        <td class="table-action">
+                                            <a href="" data-toggle="tooltip" title="Detalhes" class="tooltips"><i class="fa fa-eye"></i></a>
+
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>24/01/2022 - 22:52</td>
+                                        <td>Depósito</td>
+                                        <td>Aprovado</td>
+                                        <td>US$ 100,00</td>
+                                        <td class="table-action">
+                                            <a href="" data-toggle="tooltip" title="Detalhes" class="tooltips"><i class="fa fa-eye"></i></a>
+
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>24/01/2022 - 22:52</td>
+                                        <td>Depósito</td>
+                                        <td>Aprovado</td>
+                                        <td>US$ 100,00</td>
+                                        <td class="table-action">
+                                            <a href="" data-toggle="tooltip" title="Detalhes" class="tooltips"><i class="fa fa-eye"></i></a>
+
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>24/01/2022 - 22:52</td>
+                                        <td>Depósito</td>
+                                        <td>Aprovado</td>
+                                        <td>US$ 100,00</td>
+                                        <td class="table-action">
+                                            <a href="" data-toggle="tooltip" title="Detalhes" class="tooltips"><i class="fa fa-eye"></i></a>
+
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>24/01/2022 - 22:52</td>
+                                        <td>Depósito</td>
+                                        <td>Aprovado</td>
+                                        <td>US$ 100,00</td>
+                                        <td class="table-action">
+                                            <a href="" data-toggle="tooltip" title="Detalhes" class="tooltips"><i class="fa fa-eye"></i></a>
+
+                                        </td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div><!-- table-responsive -->
+                    </div><!-- col-md-12 -->
+
+                </div>
+                <!-- /.row (nested) -->
+            </div>
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+
